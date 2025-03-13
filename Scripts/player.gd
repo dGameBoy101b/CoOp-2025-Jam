98fed0748a3464b7b41edbcdaad51bc765ca403d
childishdelusions <imuffynz@icloud.com>
2025-03-13 18:33:01 +0800

    Improved basic player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
-@export var speed = 350
-@export var jump_strength = 700
-@export var gravity = 980
+@export var speed = 400
+@export var acceleration = 0.35
+@export var jump_strength = 800
+@export var gravity = 1500
+@export var fall_multiplier = 1.5
 
 func _physics_process(delta: float) -> void:
 	var movement_input = (Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left"))
-	velocity.x = movement_input * speed
+	velocity.x = lerpf(velocity.x, movement_input * speed, acceleration)
 	
-	if Input.is_action_pressed("jump"):
-		if is_on_floor():
-			velocity.y -= jump_strength
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_strength
 	
 	if !is_on_floor():
-		velocity.y += gravity * delta
+		if velocity.y > 0:
+			velocity.y += gravity * delta * fall_multiplier
+		else:
+			velocity.y += gravity * delta
 	
 	move_and_slide()

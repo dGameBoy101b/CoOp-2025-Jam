6b6e2fd5f2de8a0fc868691671a1fdd4fe318465
dGameBoy101b <rhysmader@gmail.com>
2025-03-17 00:37:54 +0800

    Refactored patrol ground script. Added falling. Privated direction property. Added get only direction properties. Changed to extend CharacterBody2D Moved to subfolder.

---
+++ Scripts/patrol_ground.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@export var shape : CollisionShape2D
+@export var speed : float
+@export var gravity : float
+
+var _direction : float = 1
+
+## The direction of gravity.
+var down: Vector2:
+	get:
+		return -self.up_direction.normalized()
+
+## The direction this is facing.
+var forward: Vector2:
+	get:
+		return (self.up_direction.rotated(PI/2) * self._direction).normalized()
+
+func hit_edge(delta_time: float) -> bool:
+	var motion = self.down * self.safe_margin
+	var offset = (self.speed * delta_time + self.shape.shape.get_rect().size.x) * self.forward
+	var from = self.transform.translated(offset)
+	return not self.test_move(from, motion, null, self.safe_margin)
+
+func calculate_velocity(delta_time: float) -> Vector2:
+	#falling
+	if self.is_on_floor():
+		return self.gravity * delta_time * self.down + self.velocity
+	
+	#patrolling
+	if self.is_on_wall() or self.hit_edge(delta_time):
+		self._direction *= -1
+		if self.is_on_wall():
+			print("hit wall")
+		else:
+			print("hit edge")
+	return self.speed * self.forward + self.velocity.project(self.down)
+
+func _physics_process(delta_time: float) -> void:
+	self.velocity = self.calculate_velocity(delta_time)
+	self.move_and_slide()
